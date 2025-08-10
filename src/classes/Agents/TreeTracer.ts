2b7a351fab42c6ebe5b19f78eea64ce57640d22b
Dustin <schippde@gmail.com>
2025-08-09 19:43:35 -0500

    not working - switching from tree approach

--- src/classes/Agents/TreeTracer.ts
+++ src/classes/Agents/TreeTracer.ts
@@ -1,33 +1,47 @@
 import P5 from 'p5';
-import type { TreeSystem } from '../SystemsAndManagement/TreeSystem';
+import type { BranchLevelProperties, TreeSystem } from '../SystemsAndManagement/TreeSystem';
 
 import { Vehicle } from './Vehicle';
 
 export class TreeTracer extends Vehicle {
   private treeSystem;
   private upDirection;
-  private heirarchyLevel;
-  private maxChildren;
+  public props;
   constructor(
     sketch: P5,
     coords: P5.Vector,
-    heirarchyLevel: number,
-    maxChildren: number,
+    props: BranchLevelProperties,
     treeSystem: TreeSystem,
   ) {
     super(sketch, coords);
     this.treeSystem = treeSystem;
     this.upDirection = new P5.Vector(0, 0, 1);
-    this.heirarchyLevel = heirarchyLevel;
-    this.maxChildren = maxChildren;
+    this.props = props
   }
 
   update(): TreeTracer {
+    if (this.p5.random() < this.props.unexpectedDeathProbability){
+      this.age = this.lifeExpectancy
+    }
+    
     super.update();
+    
     if ((this.age = this.lifeExpectancy)) {
-      for (let i = 0; i < this.maxChildren; i++) {}
+      // fork
     }
 
     return this;
   }
+
+  // createChildTreeTracer(maxBranchingAngle = Math.PI / 4): TreeTracer {
+  //   const childProps = this.treeSystem.getChildProps(this.props)
+
+  //   return new TreeTracer(
+  //     this.p5,
+  //     this.coords,
+  //     this.heirarchyLevel + 1,
+  //     this.maxChildren + this.heirarchyLevel,
+  //     this.treeSystem,
+  //   );
+  // }
 }
--- src/classes/SystemsAndManagement/TreeSystem.ts
+++ src/classes/SystemsAndManagement/TreeSystem.ts
@@ -5,25 +5,124 @@ import type { CoordinateSystem } from '../Geometry/CoordinateSystem';
 import { TreeTracer } from '../Agents/TreeTracer';
 
 interface TreeSystemParameters {
-  minimumBranchesAtLevel: 2;
-  branchesAtLevels: Record<number, number>;
+  branchProperties: BranchLevelProperties[]; // first branch property is lowest, last is highest
+}
+
+export interface BranchLevelProperties {
+  name: string; // name of the branch level
+  upwardTendancy: number; // -1 to 1
+  upwardStrength: number; // multiplier for upward tendancy
+  outwardTendancy: number; // -1 to 1
+  outwardStrength: number; // multiplier for outward tendancy
+  meanLifeExpectancy: number; // mean frames for branch duration, using guassian randomness
+  stdvLifeExpectancy: number; // standard deviation of life expecatancy
+  windMutliplier: number; // multiplier for the wind forces
+  gnarlMultiplier: number; // multiplier for the gnarling forces
+  ableToFork: boolean; // ability to fork to lower hierarchies at death
+  branchProbability: number; // 0 to 1 probability of branch on any given frame
+  twigProbability: number; // 0 to 1 probability of small twigs branching from large branch
+  unexpectedDeathProbability:number; // 0 to 1 probability of randomly dying
+}
+
+enum TreeStyles {
+  Default,
+}
+
+export function createTreeProperties(
+  style: TreeStyles = TreeStyles.Default,
+): TreeSystemParameters {
+  return {
+    branchProperties: [
+      {
+        name: 'Trunk',
+        upwardTendancy: 1,
+        upwardStrength: 4,
+        outwardTendancy: 0,
+        outwardStrength: 0,
+        meanLifeExpectancy: 500,
+        stdvLifeExpectancy: 50,
+        windMutliplier: 2,
+        gnarlMultiplier: 0.1,
+        ableToFork: true,
+        branchProbability: 0.05,
+        twigProbability: 0.05,
+        unexpectedDeathProbability: 0,
+      },
+      {
+        name: 'Primary Branch',
+        upwardTendancy: 1,
+        upwardStrength: 4,
+        outwardTendancy: 0,
+        outwardStrength: 0,
+        meanLifeExpectancy: 500,
+        stdvLifeExpectancy: 50,
+        windMutliplier: 2,
+        gnarlMultiplier: 0.1,
+        ableToFork: true,
+        branchProbability: 0.05,
+        twigProbability: 0.05,
+        unexpectedDeathProbability: 0
+      },
+      {
+        name: 'Secondary Branch',
+        upwardTendancy: 1,
+        upwardStrength: 4,
+        outwardTendancy: 0,
+        outwardStrength: 0,
+        meanLifeExpectancy: 500,
+        stdvLifeExpectancy: 50,
+        windMutliplier: 2,
+        gnarlMultiplier: 0.1,
+        ableToFork: true,
+        branchProbability: 0.05,
+        twigProbability: 0.05,
+        unexpectedDeathProbability: 0.01
+      },
+      {
+        name: 'Twigs',
+        upwardTendancy: 1,
+        upwardStrength: 4,
+        outwardTendancy: 0,
+        outwardStrength: 0,
+        meanLifeExpectancy: 500,
+        stdvLifeExpectancy: 50,
+        windMutliplier: 2,
+        gnarlMultiplier: 0.1,
+        ableToFork: false,
+        branchProbability: 0.05,
+        twigProbability: 0.05,
+        unexpectedDeathProbability: 0.03
+      }
+    ],
+  };
 }
 
 export class TreeSystem {
   public cs: CoordinateSystem;
-  public treeHeight: number;
-  public trunkHeight: number;
+  public props: TreeSystemParameters;
 
   public treeTracers: VehicleCollection;
 
   private p5: P5;
+  private upTarget: P5.Vector
 
-  constructor(location: CoordinateSystem, sketch: P5) {
+  constructor(
+    location: CoordinateSystem,
+    sketch: P5,
+    props = createTreeProperties(),
+  ) {
     this.cs = location;
-    this.treeHeight = 10;
-    this.trunkHeight = 4;
+    this.props = props;
     this.treeTracers = new VehicleCollection();
     this.p5 = sketch;
+    this.upTarget = this.cs.getPosition().add(this.cs.getZAxis().mult(100000))
+  }
+
+  public update(){
+    this.treeTracers.vehicles.forEach(vehicle => {
+      const branch = vehicle as TreeTracer
+      branch.seak(this.upTarget,branch.props)
+    });
   }
 
   private addTracerToCollection(tracer: TreeTracer | TreeTracer[]) {
@@ -34,8 +133,7 @@ export class TreeSystem {
     const firstBranch = new TreeTracer(
       this.p5,
       this.cs.getPosition(),
-      0,
-      2,
+      this.props.branchProperties[0],
       this,
     );
     this.treeTracers.addVehicle(firstBranch);
